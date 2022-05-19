99a854987d61cb22613b6ed949ac4b0257262f98
Giovanna Bueno Barbosa <giovanna.barbosa@bwtech.com>
2022-05-18 21:23:17 -0300

    funciona

--- index.js
+++ index.js
@@ -1,22 +1,84 @@
 const express = require('express');
-const { append } = require('express/lib/response');
+// const { append } = require('express/lib/response');
 const app = express()
 const path = require('path');
+const fs = require('fs');
 
-// const sqlite3 = require("sqlite3").verbose()
-// var db = new sqlite3.Database(__dirname + '/db/iot2.db');
+const sqlite3 = require('sqlite3')
+var db = new sqlite3.Database(__dirname + '/public/db/iot2.db');
+
+
+
+
+app.listen(3000, () => {
+    console.log("serv rodadando")
+    
+    var content = "--properties {"
+    console.log(content)
+
+    db.serialize(function() {
+        console.log("entrou no serialize")
+        
+        let properties = []
+
+        
+        db.each("select * from property where id_user = 1", function(err, row) {
+            if (err) return console.log(err.message)    
+            let property = [row.id, row.name, row.type]
+         
+
+            content = content.concat("[" + row.id + ", '" + row.name + "', '" + row.type + "'], ")
+            console.log(content)
+
+            properties.push(property)
+        
+            var write = content
+            fs.writeFile('./test.txt', write, err => {
+                if (err) {
+                  console.error(err)
+                  return
+                }
+                console.log("escreveu")
+                //file written successfully
+            })
+            // console.log(property)
+        })
+        	
+        content.concat("}")
+        console.log("")
+        // aqui eu enviaria o valor de properties
+
+        var write = content
+        fs.writeFile('./test.txt', write, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            console.log("escreveu")
+            //file written successfully
+        })
+    });
+    
+    
+
+    
+    
+    // db.close();
+    
+})
+
+
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', async function(req, res){
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/public/index.html'));
     res.send()
 })
 
 
-app.listen(3000, () => {console.log("serv rodadando")})
 //     console.log('bla')
     
 //     db.serialize(function() {
@@ -36,18 +98,9 @@ app.listen(3000, () => {console.log("serv rodadando")})
 //         })
 //         // res.json({data:properties})
 //     });
-    
+
 
     
     
 //     // db.close();
-// });
-
-
-
-
-
-// router.get('/sobre', function(req, res){
-//     res.sendFile(path.join(__dirname + '/sobre.html'));
-    
 // });
--- public/new.js
+++ public/new.js
@@ -1,3 +1,5 @@
+import { app } from '../index.js';
+
 // // import { properties } from './script.js'
 // var a = properties
 
@@ -5,31 +7,31 @@
 
 //     // })
 
-// console.log(properties);
 // import sqlite3 from "./node_modules/sqlite3"
 
-window.require = require;
+// window.require = require;
 
-const sqlite3 = require('sqlite3');
-var db = new sqlite3.Database(__dirname + "/db/iot2.db");
+// const sqlite3 = require('sqlite3');
+// var db = new sqlite3.Database(__dirname + "/db/iot2.db");
 
-document.getElementById('property').on('click', () => {
-	alert('clicou')
-})
-db.serialize(function () {
-  let properties = [];
+document.getElementById('property').onclick = function() {
+  alert(app.properties)
+  // console.log(properties);
+}
+// db.serialize(function () {
+//   let properties = [];
 
-  db.each("select * from property where id_user = 1", function (err, row) {
-    if (err) return console.log(err.message);
-    let property = {
-      linhas: [row.id, row.name, row.type],
-    };
+//   db.each("select * from property where id_user = 1", function (err, row) {
+//     if (err) return console.log(err.message);
+//     let property = {
+//       linhas: [row.id, row.name, row.type],
+//     };
 
-    properties.push(property);
-    console.log(property);
-  });
-  // res.json({data:properties})
-});
+//     properties.push(property);
+//     console.log(property);
+//   });
+//   // res.json({data:properties})
+// });
 
 // var option = document.createElement("option");
 // option.text = property[1];
@@ -41,4 +43,4 @@ db.serialize(function () {
 // select.appendChild(option);
 // console.log(select)
 
-module.exports = {}
+// module.exports = {}
