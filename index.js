ea56056da704bf346dd3f2b7631f4874cfed9add
Giovanna Bueno Barbosa <giovanna.barbosa@bwtech.com>
2022-05-21 18:26:28 -0300

    funciona menos o selectedUser

--- index.js
+++ index.js
@@ -3,104 +3,61 @@ const express = require('express');
 const app = express()
 const path = require('path');
 const fs = require('fs');
+// const user = require('./public/new.js');
 
 const sqlite3 = require('sqlite3')
 var db = new sqlite3.Database(__dirname + '/public/db/iot2.db');
 
-
-
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log("serv rodadando")
     
-    var content = "--properties {"
-    console.log(content)
-
     db.serialize(function() {
         console.log("entrou no serialize")
-        
-        let properties = []
 
-        
-        db.each("select * from property where id_user = 1", function(err, row) {
+        selectUsers()
+        selectProperties()
+    });
+    
+    // db.close();
+
+})
+
+function selectUsers(){
+    db.each("select ('[' || group_concat(json_object('id', id, 'name', name)) || ']') as 'row' from (select * from user)", function(err, row) {
+        if (err) return console.log(err.message)    
+
+        fs.writeFile('./public/users.json', String(row.row), err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            console.log("escreveu linha")
+        })
+    })
+} 
+
+function selectProperties(){
+        db.each(("select ('[' || group_concat(json_object('id', id, 'name', name, 'type', type)) || ']') as 'row' from (select * from property where id_user = ").concat("1", ")"), function(err, row) {
             if (err) return console.log(err.message)    
-            let property = [row.id, row.name, row.type]
-         
 
-            content = content.concat("[" + row.id + ", '" + row.name + "', '" + row.type + "'], ")
-            console.log(content)
-
-            properties.push(property)
-        
-            var write = content
-            fs.writeFile('./test.txt', write, err => {
+            // console.log(user.selectedUser)
+            fs.writeFile('./public/properties.json', String(row.row), err => {
                 if (err) {
                   console.error(err)
                   return
                 }
                 console.log("escreveu")
-                //file written successfully
             })
-            // console.log(property)
         })
-        	
-        content.concat("}")
-        console.log("")
-        // aqui eu enviaria o valor de properties
-
-        var write = content
-        fs.writeFile('./test.txt', write, err => {
-            if (err) {
-              console.error(err)
-              return
-            }
-            console.log("escreveu")
-            //file written successfully
-        })
-    });
-    
-    
-
-    
-    
-    // db.close();
-    
-})
-
-
-
-app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, 'public')));
+}
 
 
 app.get('/', function(req, res){
+    console.log("to no get")
     res.sendFile(path.join(__dirname + '/public/index.html'));
     res.send()
 })
 
-
-//     console.log('bla')
-    
-//     db.serialize(function() {
-
-//         let properties = []
-
-//         db.each("select * from property where id_user = 1", function(err, row) {
-//             if (err) return console.log(err.message)    
-//             let property = {
-//                 linhas: [row.id, row.name, row.type]
-//             }
-
-//             properties.push(property)
-//             console.log(property)
-
-
-//         })
-//         // res.json({data:properties})
-//     });
-
-
-    
-    
-//     // db.close();
-// });
--- public/new.js
+++ public/new.js
@@ -1,46 +1,80 @@
-import { app } from '../index.js';
+import properties from './properties.json' assert {type:'json'}
+import users from './users.json' assert {type:'json'}
 
-// // import { properties } from './script.js'
-// var a = properties
+import {createRequire} from "module"
+const requires = createRequire(import.meta.url)
+const fs =  requires("fs")
 
-//     // $("#property").on("click", function(){
+let selectedUser = ""
+window.onload = function() {
 
-//     // })
+  loadUsers()
+  // on change no user eu pego o valor do usuáruio, escrevo num txt, o outro js lê e faz a consulta das properties
+  // e no próprio change do user, já pega as infos pra carregar as propriedades
 
-// import sqlite3 from "./node_modules/sqlite3"
 
-// window.require = require;
+  // atual problema: nao consigo escrever o user selecionado num arquivo txt nesse script pra mandar pro outro. qualquer coisa eu só ignoro isso. fodase (seria um plus)
 
-// const sqlite3 = require('sqlite3');
-// var db = new sqlite3.Database(__dirname + "/db/iot2.db");
-
-document.getElementById('property').onclick = function() {
-  alert(app.properties)
+  loadProperties()
+  console.log('CLICOU!!')
   // console.log(properties);
+
+  var option = document.createElement("option");
+  option.text = properties.property1.name;
+  option.value = properties.property1.id;
+  console.log(option)
+
+  var select = document.getElementById("property");
+  console.log(select)
+  select.appendChild(option);
+  console.log(select)
 }
-// db.serialize(function () {
-//   let properties = [];
 
-//   db.each("select * from property where id_user = 1", function (err, row) {
-//     if (err) return console.log(err.message);
-//     let property = {
-//       linhas: [row.id, row.name, row.type],
-//     };
+document.getElementById('user').addEventListener('change', function(){
+  selectedUser = this.value
+  console.log(selectedUser)
+  WriteToFile(selectedUser)
+})
+// exports.selectedUser = selectedUser
 
-//     properties.push(property);
-//     console.log(property);
-//   });
-//   // res.json({data:properties})
-// });
 
-// var option = document.createElement("option");
-// option.text = property[1];
-// option.value = property[0];
-// console.log(option)
+function WriteToFile(data) {
+  fs.writeFile('./public/selectedUser.txt', String(data), err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log("escreveu selected user")
+  })
+}
 
-// var select = document.getElementById("property");
-// console.log(select)
-// select.appendChild(option);
-// console.log(select)
 
-// module.exports = {}
+// var myproperties = JSON.parse(properties);
+console.log(properties)
+
+function loadUsers(){
+  console.log('entrou no load users')
+  
+  for (let i = 0; i < users.length; i++) {
+    var option = document.createElement("option");
+    option.text = users[i].name;
+    option.value = users[i].id;
+    console.log(option.text)
+    
+    var select = document.getElementById("user");
+    select.appendChild(option);
+  }
+}
+
+function loadProperties(){
+  for (let i = 0; i < properties.length; i++) {
+    var option = document.createElement("option");
+    option.text = properties[i].name;
+    option.value = properties[i].id;
+    console.log(option.text)
+    
+    var select = document.getElementById("property");
+    select.appendChild(option);
+  }
+}
+
