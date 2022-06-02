1a84b303aade652b9ebc5638a04801ea8b15905e
Giovanna Bueno Barbosa <giovanna.barbosa@bwtech.com>
2022-06-01 21:12:24 -0300

    agr vai

--- index.js
+++ index.js
@@ -2,19 +2,95 @@ const express = require('express');
 // const { append } = require('express/lib/response');
 const app = express()
 const path = require('path');
-// const user = require('./public/new.js');
+
+
+var jsdom = require('jsdom');
+$ = require('jquery')(new jsdom.JSDOM().window);
+
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, '/views'))
+
+let selectedProperty = ""
+let initDate = ""
+let finalDate = ""
+let selectedTypeOfGrouping = ""
+
+let properties = []
 
 const sqlite3 = require('sqlite3')
-// var db = new sqlite3.Database(__dirname + '/public/db/iot2.db');
+var db = new sqlite3.Database(__dirname + '/public/db/iot2.db');
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
-    console.log("serv rodadando")  
+    console.log("serv rodadando")
+    
+    db.serialize(function() {
+        console.log("entrou no serialize")
+        selectProperties()
+    });
+
+    $(function (){
+       console.log("oi")
+    })
+
+
+    $('#property').on('change', function(){
+        selectedProperty = this.value
+        console.log(selectedProperty)
+    })
+
+    $('#typeOfGroup').on('change', function(){
+        selectedTypeOfGrouping = this.value 
+        console.log(selectedTypeOfGrouping)
+    })
+
+    $('#fromDate').on('change', function(){
+        initDate = this.value
+        console.log('fromDate changed to ' + initDate)
+        $('#toDate').min = initDate
+    })
+
+
+    $('#toDate').on('change', function(){
+        finalDate = this.value
+        console.log('toDate changed to ' + finalDate)
+        // var fromDate = document.getElementById("fromDate");
+        $('#fromDate').max = finalDate
+    })
+
+    $('#showGraph').on('click', function() {
+        if (selectedProperty == "" || (initDate == "" && finalDate == "") || selectedTypeOfGrouping == "") {
+            alert('cannot show the graph without selecting the property')
+        } 
+        
+        console.log('will show the graph') 
+    });
+
 })
 
+function selectProperties(){
+    db.each("select property.id as 'id', property.name as 'name', property.type as 'type' from property where id_user = 1", function(err, row) {
+        if (err) return console.log(err.message)    
+
+        let data = {id: row.id, name: row.name, type: row.type}
+        console.log(data)
+        properties.push(data)
+    })
+}
+
+//view n tá carregando
+
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-    res.send()
+
+    res.render(path.resolve(__dirname + '/views/graph', { 
+        properties: properties
+    }))
+    // res.sendFile(path.join(__dirname + '/public/index.html'));
+    // res.send()
+})
+
+app.post('/', function(req, res) {
+    res.redirect("/")
 })
--- public/main.js
+++ public/main.js
@@ -1,86 +1,69 @@
-// import * as properties from './properties.json' assert {type:'json'}
-// import users from './users.json' assert {type:'json'}
-// import sqlite3 from './db/sqlite3.def';
-const sqlite3 = require('../index.js/sqlite3');
-var db = new sqlite3.Database(__dirname + '/public/db/iot2.db');
+import properties from './properties.json' assert {type:'json'}
 
-// import * as testModule from "./testModule.js";
-// import { test2module } from "./testModule2.js";
+// let selectedProperty = ""
+// let initDate = ""
+// let finalDate = ""
+// let selectedTypeOfGrouping = ""
 
-let selectedProperty = ""
-let initDate = ""
-let finalDate = ""
-let selectedTypeOfGrouping = ""
+// window.onload = function() {
+//   // loadProperties()
+// }
 
-window.onload = function() {
-  loadProperties()
-}
+// document.getElementById('property').addEventListener('change', function(){
+//   selectedProperty = this.value
+//   console.log(selectedProperty)
+// })
 
-document.getElementById('property').addEventListener('change', function(){
-  selectedProperty = this.value
-  console.log(selectedProperty)
-})
+// document.getElementById('typeOfGroup').addEventListener('change', function(){
+//   selectedTypeOfGrouping = this.value
+//   console.log(selectedTypeOfGrouping)
+// });
 
-
-
-document.getElementById('typeOfGroup').addEventListener('change', function(){
-  selectedTypeOfGrouping = this.value
-  console.log(selectedTypeOfGrouping)
-});
-
-document.getElementById('fromDate').addEventListener('change', function(){
-  initDate = this.value
-  console.log('fromDate changed to ' + initDate)
-  // var toDate = document.getElementById("toDate");
-  document.getElementById("toDate").min = initDate
-})
+// document.getElementById('fromDate').addEventListener('change', function(){
+//   initDate = this.value
+//   console.log('fromDate changed to ' + initDate)
+//   // var toDate = document.getElementById("toDate");
+//   document.getElementById("toDate").min = initDate
+// })
 
 
-document.getElementById('toDate').addEventListener('change', function(){
-  finalDate = this.value
-  console.log('toDate changed to ' + finalDate)
-  // var fromDate = document.getElementById("fromDate");
-  document.getElementById("fromDate").max = finalDate
-})
+// document.getElementById('toDate').addEventListener('change', function(){
+//   finalDate = this.value
+//   console.log('toDate changed to ' + finalDate)
+//   // var fromDate = document.getElementById("fromDate");
+//   document.getElementById("fromDate").max = finalDate
+// })
 
-document.getElementById('showGraph').addEventListener('click', function() {
-  // if (selectedProperty == "" || (initDate == "" && finalDate == "") || selectedTypeOfGrouping == "") {
-  //   alert('cannot show the graph without selecting the property')
-  // } 
+// document.getElementById('showGraph').addEventListener('click', function() {
+//   if (selectedProperty == "" || (initDate == "" && finalDate == "") || selectedTypeOfGrouping == "") {
+//     alert('cannot show the graph without selecting the property')
+//   } 
   
-  console.log('will show the graph') 
-  //mandar aqui as informações pro sqlite de alguma forma fazer isso funcionar 
-  alert("oi")
-
-
-
-  db.serialize(function() {
-    db.each("select ('[' || group_concat(json_object('id', id, 'name', name, 'type', type)) || ']') as 'row' from (select * from property where id_user = 1)", function(err, row) {
-      if (err) return console.log(err.message)    
+//   console.log('will show the graph') 
+//   //mandar aqui as informações pro sqlite de alguma forma fazer isso funcionar 
+//   // db.serialize(function() {
+//   //   db.each("select ('[' || group_concat(json_object('id', id, 'name', name, 'type', type)) || ']') as 'row' from (select * from property where id_user = 1)", function(err, row) {
+//   //     if (err) return console.log(err.message)    
       
-      console.log("sqlite do new.js")
-    })
-  });
+//   //     console.log("sqlite do new.js")
+//   //   })
+//   // });
 
+//   alert("oi")
 
-});
+// });
 
-function loadProperties(){
+// console.log(properties)
 
-  db.each("select ('[' || group_concat(json_object('id', id, 'name', name, 'type', type)) || ']') as 'row' from (select * from property where id_user = 1)", function(err, row) {
-    if (err) return console.log(err.message)    
+// function loadProperties(){
+//   for (let i = 0; i < properties.length; i++) {
+//     var option = document.createElement("option");
+//     option.text = properties[i].name;
+//     option.value = properties[i].id;
+//     console.log(option.text)
     
-    var option = document.createElement("option");
-    option.text = row.row[i].name;
-    option.value = row.row[i].id;
-    console.log(option.text)
-    
-    var select = document.getElementById("property");
-    select.appendChild(option);
-  
-  })
-
-  // for (let i = 0; i < properties.length; i++) {
-  // }
-}
+//     var select = document.getElementById("property");
+//     select.appendChild(option);
+//   }
+// }
 
